0effd7ff407133131ea2f4205fa35971ca83d9ff
Shawn Mathews <mathewsms320@outlook.com>
2023-04-08 21:54:46 -0700

    fragments, rendering, lists, events

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,27 @@
+import { MouseEvent } from 'react';
+
+function ListGroup() {
+	let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
+
+	// Event Handler
+	const handleClick = (event: MouseEvent) => console.log(event);
+
+	return (
+		<>
+			<h1>List</h1>
+			{items.length === 0 && <p>No items found</p>}
+			<ul className='list-group'>
+				{items.map((item, index) => (
+					<li
+						key={item}
+						className='list-group-item'
+						onClick={(event) => console.log(event)}>
+						{item}
+					</li>
+				))}
+			</ul>
+		</>
+	);
+}
+
+export default ListGroup;
