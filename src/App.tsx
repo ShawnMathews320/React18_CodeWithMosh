cced83d14b32afd3c5656e2408638f025fe39f33
Shawn Mathews <mathewsms320@outlook.com>
2023-04-18 16:14:26 -0700

    passing children

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import ListGroup from './components/ListGroup';
+import { Alert } from './components/Alert';
+import { Button } from './components/Button';
+
 function App() {
-	let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
-
-	const handleSelectItem = (item: string) => {
-		console.log(item);
-	};
-
 	return (
 		<div>
-			<ListGroup
-				items={items}
-				heading='Cities'
-				onSelectItem={handleSelectItem}
-			/>
+			<Button color='success' onClick={() => console.log('Clicked')}>
+				My Button
+			</Button>
 		</div>
 	);
 }
