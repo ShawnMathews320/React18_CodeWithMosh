c9e5b3c30ec25312dc93eac813cc03bdbe35deda
Shawn Mathews <mathewsms320@outlook.com>
2023-04-18 20:03:18 -0700

    vanilla css

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import { useState } from 'react';
 import { Alert } from './components/Alert';
 import { Button } from './components/Button';
+import ListGroup from './components/ListGroup';
+import './App.css';
 
 function App() {
 	const [alertVisible, setAlertVisibility] = useState(false);
+	const items = ['New York', 'Los Angeles', 'San Francisco'];
 
 	return (
 		<div>
-			{alertVisible && (
+			<ListGroup
+				heading='Miami'
+				items={items}
+				onSelectItem={() => {}}></ListGroup>
+			{/* {alertVisible && (
 				<Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>
 			)}
 			<Button color='success' onClick={() => setAlertVisibility(true)}>
 				My Button
-			</Button>
+			</Button> */}
 		</div>
 	);
 }
