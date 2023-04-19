cadb09db924b77e9eb033c79b66af66ca11640c9
Shawn Mathews <mathewsms320@outlook.com>
2023-04-18 17:15:05 -0700

    buttons and alerts

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,9 +1,21 @@
 import { ReactNode } from 'react';
 
 interface Props {
-	children: ReactNode;
+	children: string;
+	color?: 'primary' | 'secondary' | 'danger' | 'success';
+	onClose: () => void;
 }
 
-export const Alert = ({ children }: Props) => {
-	return <div className='alert alert-primary'>{children}</div>;
+export const Alert = ({ children, color = 'primary', onClose }: Props) => {
+	return (
+		<div className='alert alert-primary alert-dismissible'>
+			{children}{' '}
+			<button
+				onClick={onClose}
+				type='button'
+				className='btn-close'
+				data-bs-dismiss='alert'
+				aria-label='Close'></button>
+		</div>
+	);
 };
